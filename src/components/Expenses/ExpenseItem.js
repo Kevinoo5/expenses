dfd690ed9e9b1b71f38ac0dbf196d37c39a712dc
Kevinoo5 <kevin.noormets@voco.ee>
2023-04-12 09:40:21 +0300

    Added useState hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,13 @@
+import React, {useState} from "react";
 import "./ExpenseItem.css"
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.expenseData.title);
 
     const clickHandler = () => {
-        console.log("Clicked!");
+        setTitle("Updated!");
+        console.log(title);
     }
 
     return (
